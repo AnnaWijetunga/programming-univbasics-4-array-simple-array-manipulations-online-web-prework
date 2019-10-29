48be46614ba4a5b8aff1703d0f159a6a5cb42db3
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-29 01:17:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, element)  
-  element.insert(4, new)
+def using_insert(array, new_element)  
+  element.insert(4, new_element)
 end 
   
   
